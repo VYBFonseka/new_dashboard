32cdf2cdfd78da5a6662a49612a99c2d8b953277
VYBFonseka <en91441@sjp.ac.lk>
2023-05-03 10:35:05 +0530

    start end & firebase config

--- src/components/StartEndBtn/StartEndBtn.jsx
+++ src/components/StartEndBtn/StartEndBtn.jsx
@@ -4,6 +4,7 @@ import './startEndBtn.css'
 export default function StartEndBtn() {
   return (
     <div className='startEndBtn'>
+    <div className="startEndBtnContainer">
       <Stack direction="row" spacing={3}>
       
       <Button variant="contained" color="success">
@@ -13,6 +14,7 @@ export default function StartEndBtn() {
         End Test
       </Button>
     </Stack>  
+    </div>
     </div>
   )
 }
