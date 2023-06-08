669902b2e85e5d67741cad58f1964e620fd4c465
Gihan Lakshan <gihanlakshan276@gmail.com>
2023-06-09 00:01:13 +0530

    charts

--- src/components/ResGraph/ResGraph.jsx
+++ src/components/ResGraph/ResGraph.jsx
@@ -12,59 +12,41 @@ import {
   Legend
 } from "recharts";
 
-const data = [
-  {
-    name: "001",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
-
-
 export default function ResGraph() {
+
+  const [chartData,setChartData] = useState([]);
+
+  const empCollectionRef = collection(storedb, "aranayake");
+
+  useEffect(() => {
+      getList();
+  },[]);
+
+  const getList = async () => {
+    const data = await getDocs(empCollectionRef);
+    let stats = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+    let chartPoints = [];
+
+    stats.forEach((item) => {
+      let d = {
+        name: item.timeSlot,
+        Humidity: item.humidityAtmos,
+        Temperature: item.temperatureAtmos,
+        ObjectTemperature: item.porePressure2,
+      }
+
+      chartPoints.push(d);
+    })
+
+    setChartData(chartPoints);
+  };
 
   return (
     <LineChart
       width={1000}
       height={300}
-      data={data}
+      data={chartData}
       margin={{
         top: 5,
         right: 30,
@@ -73,25 +55,25 @@ export default function ResGraph() {
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis />
+      <XAxis dataKey="name" />
       <YAxis />
       <Tooltip />
       <Legend />
       <Line
         type="monotone"
-        dataKey="pv"
+        dataKey="Humidity"
         stroke="#8884d8"
         activeDot={{ r: 5 }}
       />
       <Line 
         type="monotone" 
-        dataKey="amt" 
+        dataKey="Temperature" 
         stroke="#8884d8" 
         activeDot={{ r: 5 }}
       />
       <Line 
         type="monotone" 
-        dataKey="uv" 
+        dataKey="ObjectTemperature" 
         stroke="#82ca9d" 
         activeDot={{ r: 5 }} 
       />
